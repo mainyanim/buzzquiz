d97ca9722c0ea316c1e0544231acc99bd38820e6
mainyanim <alicejarmusch@gmail.com>
2016-12-14 13:42:33 +0200

    first steps

---
+++ js/foo.js
@@ -0,0 +1,4 @@
+document.getElementById("startBtn").addEventListener('click', goNext);
+function goNext() {
+  document.getElementById("startImg").src = "http://cdn3.thr.com/sites/default/files/2015/07/rick_and_morty_s02_still.jpg";
+}
